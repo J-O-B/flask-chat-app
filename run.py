196b5d580230aeeb9ea611af9707c6268a280c7d
J-O-B <Jonathanmichaelobrien@gmail.com>
2020-12-10 01:28:58 +0000

    send messages and log user complete

--- run.py
+++ run.py
@@ -2,23 +2,29 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+messages = []
+
+
+def add_messages(username, message):
+    messages.append("{}: {}".format(username, message))
 
 
 @app.route("/")
 def index():
-    """Main page with instructions"""
-    return "To send a message use /USERNAME/MESSAGE"
+    """ Main page with instructions """
+    return "To send a message use: /USERNAME/MESSAGE"
 
 
 @app.route("/<username>")
 def user(username):
-    return "Hi" + username
+    """Display chat messages"""
+    return "Welcome {}! Your Latest Messages: {}".format(username, messages)
 
 
-@app.route("/username/<message>")
+@app.route("/<username>/<message>")
 def send_message(username, message):
+    """Create new chat and redir to chat page"""
     return "{0}: {1}".format(username, message)
 
-if __name__ == "__main__":
-    app.run(host=os.getenv("IP"), port=int(os.getenv(
-        "PORT")), debug=True)
+
+app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
